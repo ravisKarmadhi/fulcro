f8b5571277b56570abbc882b9b621ad4097f5fdb
jigishachavda <jigishachavda1011@gmail.com>
2024-03-16 09:41:49 +0530

    careers page and career apply canvas design with responsive

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -20403,7 +20403,48 @@ var App = /*#__PURE__*/function () {
   }
   _createClass(App, [{
     key: "init",
-    value: function init() {}
+    value: function init() {
+      $(document).ready(function () {
+        var $buttons = $(".vacancies-btn");
+        $buttons.each(function () {
+          var $button = $(this);
+          var $content = $button.closest(".vacancies-cards").find(".vacancies-content");
+          var textHeight = $content[0].scrollHeight;
+          var defaultHeight = $(window).width() <= 393 ? 300 : $(window).width() <= 426 ? 150 : $(window).width() <= 768 ? 80 : 244;
+          $content.css({
+            "max-height": defaultHeight,
+            "overflow": "hidden"
+          });
+          $button.on("click", function () {
+            if ($content.hasClass("active")) {
+              $content.animate({
+                "max-height": defaultHeight
+              }, 500, function () {
+                $content.removeClass("active");
+                $button.html('Read More'); // Change button text to "Read More"
+              });
+            } else {
+              $content.animate({
+                "max-height": textHeight
+              }, 500, function () {
+                $content.addClass("active");
+                $button.html('Read Less'); // Change button text to "Read Less"
+              });
+            }
+          });
+        });
+
+        // // Add event listener for window resize to update defaultHeight
+        // $(window).resize(function () {
+        //   $buttons.each(function () {
+        //     var $button = $(this);
+        //     var $content = $button.closest(".vacancies-cards").find(".vacancies-content");
+        //     var defaultHeight = $(window).width() <= 393 ? 180 : $(window).width() <= 426 ? 150 : ($(window).width() <= 768 ? 80 : 70);
+        //     $content.css({ "max-height": defaultHeight });
+        //   });
+        // });
+      });
+    }
   }]);
   return App;
 }();
--- src/js/parts/app.js
+++ src/js/parts/app.js
@@ -1,5 +1,44 @@
 export class App {
   init() {
+    $(document).ready(function () {
+      var $buttons = $(".vacancies-btn");
 
+      $buttons.each(function () {
+        var $button = $(this);
+        var $content = $button.closest(".vacancies-cards").find(".vacancies-content");
+        var textHeight = $content[0].scrollHeight;
+        var defaultHeight = $(window).width() <= 393 ? 300 : $(window).width() <= 426 ? 150 : ($(window).width() <= 768 ? 80 : 244);
+
+        $content.css({ "max-height": defaultHeight, "overflow": "hidden" });
+
+        $button.on("click", function () {
+          if ($content.hasClass("active")) {
+            $content.animate({
+              "max-height": defaultHeight
+            }, 500, function () {
+              $content.removeClass("active");
+              $button.html('Read More'); // Change button text to "Read More"
+            });
+          } else {
+            $content.animate({
+              "max-height": textHeight
+            }, 500, function () {
+              $content.addClass("active");
+              $button.html('Read Less'); // Change button text to "Read Less"
+            });
+          }
+        });
+      });
+
+      // // Add event listener for window resize to update defaultHeight
+      // $(window).resize(function () {
+      //   $buttons.each(function () {
+      //     var $button = $(this);
+      //     var $content = $button.closest(".vacancies-cards").find(".vacancies-content");
+      //     var defaultHeight = $(window).width() <= 393 ? 180 : $(window).width() <= 426 ? 150 : ($(window).width() <= 768 ? 80 : 70);
+      //     $content.css({ "max-height": defaultHeight });
+      //   });
+      // });
+    });
   }
 }
